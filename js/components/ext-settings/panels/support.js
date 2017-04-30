90b3e5f1139b4bb85527dad040bbf659c80b8ad7
Dan Bovey <dan@danbovey.uk>
2017-04-30 17:15:57 +0100

    Support settings

--- js/components/ext-settings/panels/support.js
+++ js/components/ext-settings/panels/support.js
@@ -3,17 +3,35 @@ module.exports = Storage => {
     content.classList.add('Mess-setting-panel');
     content.id = 'Mess-panel-support';
 
-    const comingSoon = document.createElement('div');
-    comingSoon.classList.add('Mess-setting-group');
+    const submitBug = document.createElement('div');
+    submitBug.classList.add('Mess-setting-group');
     let heading = document.createElement('h4');
-    heading.textContent = 'Working on it!';
-    comingSoon.appendChild(heading);
+    heading.textContent = 'Issues or feedback?';
+    submitBug.appendChild(heading);
 
-    const p = document.createElement('p');
-    p.textContent = 'Tip jar and GitHub contribution instructions coming soon';
-    comingSoon.appendChild(p);
+    let p = document.createElement('p');
+    const link = document.createElement('a');
+    link.setAttribute('href', 'http://github.com/danbovey/Mess/issues');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener');
+    link.textContent = 'Submit a bug or feature request on GitHub';
+    p.appendChild(link);
+    submitBug.appendChild(p);
 
-    content.appendChild(comingSoon);
+    content.appendChild(submitBug);
+
+    const hr = document.createElement('hr');
+    content.appendChild(hr);
+
+    const tipJar = document.createElement('div');
+    tipJar.classList.add('Mess-setting-group');
+    heading = document.createElement('h4');
+    heading.textContent = 'Tip jar';
+    tipJar.appendChild(heading);
+    p = document.createElement('p');
+    p.textContent = 'A tip jar to support the development of Mess is in the works!';
+    tipJar.appendChild(p);
+    content.appendChild(tipJar);
 
     return content;
 };
